acf477adcfb9fe7e8d28ee92407136de6136b6e8
Kzu <alvaro.vega.pavez@gmail.com>
2024-11-17 02:59:13 -0300

    add: usuario service

---
+++ src/main/java/cl/duoc/app/service/UsuarioService.java
@@ -0,0 +1,19 @@
+package cl.duoc.app.service;
+
+
+import cl.duoc.app.model.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UsuarioService {
+
+    List<Usuario> findAll();
+
+    Optional<Usuario> findById(Long id);
+
+    Usuario save(Usuario usuario);
+
+    void delete(Long id);
+
+}
