ab64808aa3c8a88c6ce2f0a6eecb1731a9e3f619
Kzu <alvaro.vega.pavez@gmail.com>
2024-11-17 02:58:59 -0300

    add: usuario service impl

---
+++ src/main/java/cl/duoc/app/service/UsuarioServiceImpl.java
@@ -0,0 +1,40 @@
+package cl.duoc.app.service;
+
+import cl.duoc.app.model.Usuario;
+import cl.duoc.app.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+
+    final UsuarioRepository repository;
+
+    @Autowired
+    public UsuarioServiceImpl(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<Usuario> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Optional<Usuario> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Usuario save(Usuario usuario) {
+        return repository.save(usuario);
+    }
+
+    @Override
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
